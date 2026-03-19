8efdfe4a92fa60533bb25641d03d51bbc9c04ae2
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-19 15:24:25 +0100

    wip

---
+++ rector.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use Mpietrucha\Support\Filesystem\Path;
+use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\Expression\RemoveDeadStmtRector;
+use Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        Path::get('src'),
+    ])
+    ->withSkip([
+        RemoveUselessParamTagRector::class,
+        RemoveDeadStmtRector::class,
+        RemoveNonExistingVarAnnotationRector::class,
+    ])
+    ->withRules([
+        StaticClosureRector::class,
+        StaticArrowFunctionRector::class,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        typeDeclarationDocblocks: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        carbon: true
+    )
+    ->withPhpSets(php85: true)
+    // ->withBootstrapFiles([
+    //     Path::build('vendor/larastan/larastan/bootstrap.php'),
+    // ])
+    ->withPhpstanConfigs([
+        Path::get('phpstan.neon'),
+    ]);
